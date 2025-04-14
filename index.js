ba9f069da9798e9da77d723d0bfed4e8777d425d
daDude123 <153016828+Coding-master5@users.noreply.github.com>
2025-04-14 10:09:59 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+var elems = [];
+var Game = function(name) {
+  this.name = name;
+  this.refElem = document.createElement('canvas');
+};
+Game.prototype.add = function(type, x, y, id) {
+  var elem = document.createElement(type);
+  elem.id = id;
+  elem.top = y;
+  elem.left = x;
+  this.refElem.appendChild(elem);
+  elems.append(id);
+};
+Game.prototype.chain = function(event, elementId, func) {
+  if (event == 'click') {
+    document.getElementById(elementId).addEventListener('click', function() {
+      func();
+    });
+  }
+  else if (event == 'dblclick') {
+    document.getElementById(elementId).addEventListener('dblclick', function() {
+      func();
+    });
+  }
+  else if (event == 'mouseover') {
+    document.getElementById(elementId).addEventListener('mouseover', function() {
+      func();
+    });
+  }
+  else if ('keypress' in event) {
+    document.addEventListener('keypress', function(e) {
+      if (e.keyCode == event.substr(9, event.length)) {
+        func();
+      }
+    });
+  }
+};
+Game.prototype.run = function() {
+  document.body.appendChild(this.refElem);
+};
