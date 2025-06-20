91884b6650476c9c2ab1f38ad1c5db73258c0735
daDude123 <153016828+Coding-master5@users.noreply.github.com>
2025-06-20 16:57:30 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,21 +5,14 @@ class Game {
     canvas.height = h;
     this.ctx = canvas.getContext('2d');
     this.canvas = canvas;
-    canvas.style.border = '1px solid #000';
-    
-    this.camX = 0;
-    this.camY = 0;
-    this.camZ = 1;
-
-    this.fov = 75;
     
     document.body.appendChild(canvas);
   }
   #drawTriangle(x1, y1, z1, x2, y2, z2, x3, y3, z3, c) {
     this.ctx.beginPath();
-    this.ctx.moveTo(((x1+this.camX)/(z1+this.camZ)+this.canvas.width/2)*this.fov, ((y1+this.camY)/(z1+this.camZ)+this.canvas.height/2)*this.fov);
-    this.ctx.lineTo(((x2+this.camX)/(z2+this.camZ)+this.canvas.width/2)*this.fov, ((y2+this.camY)/(z2+this.camZ)+this.canvas.height/2)*this.fov);
-    this.ctx.lineTo(((x3+this.camX)/(z3+this.camZ)+this.canvas.width/2)*this.fov, ((y3+this.camY)/(z3+this.camZ)+this.canvas.height/2)*this.fov);
+    this.ctx.moveTo(x1/z1+this.canvas.width/2, y1/z1+this.canvas.height/2);
+    this.ctx.lineTo(x2/z2+this.canvas.width/2, y2/z2+this.canvas.height/2);
+    this.ctx.lineTo(x3/z3+this.canvas.width/2, y3/z3+this.canvas.height/2);
     this.ctx.closePath();
     this.ctx.fillStyle = c || 'black';
     this.ctx.fill();
@@ -35,8 +28,5 @@ class Game {
     this.#drawPlane(x2, y2, z2, x3, y3, z3, x7, y7, z7, x6, y6, z6, c);
     this.#drawPlane(x4, y4, z4, x3, y3, z3, x7, y7, z7, x8, y8, z8, c);
     this.#drawPlane(x1, y1, z1, x4, y4, z4, x8, y8, z8, x5, y5, z5, c);
-  }
-  setCameraPos(x, y, z, xRot, yRot, zRot) {
-    [this.camX, this.camY, this.camZ] = [x, y, z];
   }
 }
