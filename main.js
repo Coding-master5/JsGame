f087834a44ddce2128a7e5bf90edafddbe4b2220
daDude123 <153016828+Coding-master5@users.noreply.github.com>
2025-06-19 22:39:57 -0400

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ class Game {
     canvas.height = h;
     this.ctx = canvas.getContext('2d');
   }
-  function drawQuad(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4, x5, y5, z5, x6, y6, z6, x7, y7, z7, x8, y8, z8, c) {
+  drawQuad(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4, x5, y5, z5, x6, y6, z6, x7, y7, z7, x8, y8, z8, c) {
   	drawPlane(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4, c);
     drawPlane(x5, y5, z5, x6, y6, z6, x7, y7, z7, x8, y8, z8, c);
     drawPlane(x1, y1, z1, x2, y2, z2, x6, y6, z6, x5, y5, z5, c);
